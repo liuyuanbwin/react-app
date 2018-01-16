910504a0e11b2842310301fa297766536d5d68c8
LeoLau <liuyuansix@163.com>
2018-01-16 17:27:35 +0800

    clickCounter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ClickCounter from './ClickCounters';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ClickCounter/>,
+  document.getElementById('root')
+);
 registerServiceWorker();
